8bf66a5a283c2cfb7a6b765b5d27b94b493b4b85
Mike Thompson <mike.thompson@day8.com.au>
2016-07-19 00:08:56 +1000

    WIP

--- src/day8/re_frame/forward_events_fx.cljs
+++ src/day8/re_frame/forward_events_fx.cljs
@@ -1,8 +1,8 @@
 (ns day8.re-frame.forward-events-fx
-  (:require [re-frame.core]))
+  (:require [re-frame.core :as re-frame]))
 
 
-(re-frame.core/reg-fx
+(re-frame/def-fx
   :forward-events
   (let [id->listen-fn (atom {})
         process-one-entry (fn [{:as m :keys [unlisten listen events dispatch-to]}]
@@ -11,17 +11,17 @@
                                                  (= #{:listen :events :dispatch-to} (-> m keys set))) "re-frame: effects handler for :forward-events given wrong map keys")]
                               (if unlisten
                                 (let [f (@id->listen-fn unlisten)
-                                      _  (assert (some? f) (str ":forward-events  asked to unregister an unknown id: " id))]
-                                  (re-frame.core/remove-post-event-callback f)
+                                      _  (assert (some? f) (str ":forward-events  asked to unregister an unknown id: " unlisten))]
+                                  (re-frame/remove-post-event-callback f)
                                   (swap! id->listen-fn dissoc unlisten))
                                 (let [post-event-callback-fn  (fn [event-v _]
                                                                 (when (events (first event-v))
-                                                                  (re-frame.core/dispatch (conj dispatch-to event-v))))]
-                                  (re-frame.core/add-post-event-callback  post-event-callback-fn)
+                                                                  (re-frame/dispatch (conj dispatch-to event-v))))]
+                                  (re-frame/add-post-event-callback  post-event-callback-fn)
                                   (swap! id->listen-fn assoc listen post-event-callback-fn)))))]
     (fn [val]
       (cond
         (map? val) (process-one-entry val)
         (list? val) (doall (map process-one-entry val))
-        :else (js/console.error  ":forward-events expected a map or a list of maps, but got: " val))
+        :else (re-frame/console :error  ":forward-events expected a map or a list of maps, but got: " val))
       )))
--- test/re_frame_forward_events_fx/test_runner.cljs
+++ test/re_frame_forward_events_fx/test_runner.cljs
@@ -1,10 +1,10 @@
-(ns re-frame-forward-events-fx.test-runner
+(ns day8.re-frame.forward-events-fx.test-runner
   (:require
     [cljs.test         :as cljs-test :include-macros true]
     [jx.reporter.karma :as karma :include-macros true]
     [devtools.core     :as devtools]
     ;; Test Namespaces -------------------------------
-    [re-frame-forward-events-fx.core-test])
+    [day8.re-frame.forward-events-fx.core-test])
   (:refer-clojure :exclude (set-print-fn!)))
 
 (enable-console-print!)
@@ -17,11 +17,11 @@
 
 (defn ^:export run-html-tests []
   (cljs-test/run-tests
-    're-frame-forward-events-fx.core-test))
+    'day8.re-frame.forward-events-fx.core-test))
 
 ;; ---- KARMA  -----------------------------------------------------------------
 
 (defn ^:export run-karma [karma]
   (karma/run-tests
     karma
-    're-frame-forward-events-fx.core-test))
+    'day8.re-frame.forward-events-fx.core-test))
