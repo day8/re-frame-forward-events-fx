afe3a5c259a820ecfb833f0a1fa763e7a2dc5044
Mike Thompson <mike.thompson@day8.com.au>
2016-07-19 13:59:26 +1000

    Swap to `day8.re-frame` based namespace.   Add working tests.

--- src/day8/re_frame/forward_events_fx.cljs
+++ src/day8/re_frame/forward_events_fx.cljs
@@ -5,20 +5,20 @@
 (re-frame/def-fx
   :forward-events
   (let [id->listen-fn (atom {})
-        process-one-entry (fn [{:as m :keys [unlisten listen events dispatch-to]}]
+        process-one-entry (fn [{:as m :keys [unregister register events dispatch-to]}]
                             (let [_  (assert (map? m) (str "re-frame: effects handler for :forward-events expected a map or a list of maps. Got: " m))
-                                  _  (assert (or (= #{:unlisten} (-> m keys set))
-                                                 (= #{:listen :events :dispatch-to} (-> m keys set))) "re-frame: effects handler for :forward-events given wrong map keys")]
-                              (if unlisten
-                                (let [f (@id->listen-fn unlisten)
-                                      _  (assert (some? f) (str ":forward-events  asked to unregister an unknown id: " unlisten))]
+                                  _  (assert (or (= #{:unregister} (-> m keys set))
+                                                 (= #{:register :events :dispatch-to} (-> m keys set))) (str "re-frame: effects handler for :forward-events given wrong map keys" (-> m keys set)))]
+                              (if unregister
+                                (let [f (@id->listen-fn unregister)
+                                      _  (assert (some? f) (str ":forward-events  asked to unregister an unknown id: " unregister))]
                                   (re-frame/remove-post-event-callback f)
-                                  (swap! id->listen-fn dissoc unlisten))
+                                  (swap! id->listen-fn dissoc unregister))
                                 (let [post-event-callback-fn  (fn [event-v _]
                                                                 (when (events (first event-v))
                                                                   (re-frame/dispatch (conj dispatch-to event-v))))]
                                   (re-frame/add-post-event-callback  post-event-callback-fn)
-                                  (swap! id->listen-fn assoc listen post-event-callback-fn)))))]
+                                  (swap! id->listen-fn assoc register post-event-callback-fn)))))]
     (fn [val]
       (cond
         (map? val) (process-one-entry val)
--- test/re_frame_forward_events_fx/core_test.cljs
+++ test/re_frame_forward_events_fx/core_test.cljs
@@ -1,11 +1,44 @@
 (ns day8.re-frame.forward-events-fx.core-test
   (:require [cljs.test :refer-macros [is deftest]]
-            [day8.re-frame.forward-events-fx :as core]))
+            [day8.re-frame.forward-events-fx]
+            [re-frame.core :as re-frame]
+            [re-frame.router]))
 
-(deftest stub
-  "stub test to excercise test framework and devtools"
-  (let [data {:feed {:flint 5}}]
-    (js-debugger))
-  (is (= 5 5)))
+(deftest test1
+  (let [dispatched-events  (atom #{})
+        fake-dispatch      (fn [event]
+                             (swap! dispatched-events conj event))]
 
+    ;; setup effects handler
+    (re-frame/def-event-fx
+      :register-test
+      (fn [world event]
+        {:forward-events {:register  :my-id
+                          :events     #{:1  :2 :11 :22}
+                          :dispatch-to [:later :on ]}}))
 
+    (re-frame/def-event-fx
+      :unregister-test
+      (fn [world event]
+        {:forward-events {:unregister  :my-id}}))
+
+    (re-frame/def-event :1 (fn [db _] db))
+    (re-frame/def-event :2 (fn [db _] db))
+    (re-frame/def-event :3 (fn [db _] db))
+    (re-frame/def-event :4 (fn [db _] db))
+    (re-frame/def-event :11 (fn [db _] db))
+    (re-frame/def-event :22 (fn [db _] db))
+
+    (re-frame/def-event :later (fn [db _] db))
+
+    (with-redefs [re-frame/dispatch fake-dispatch]
+                 (re-frame/dispatch-sync [:register-test])
+                 (re-frame/dispatch-sync [:4])
+                 (re-frame/dispatch-sync [:3])
+                 (re-frame/dispatch-sync [:2])
+                 (re-frame/dispatch-sync [:1])
+                 (re-frame/dispatch-sync [:unregister-test])
+                 (re-frame/dispatch-sync [:11])
+                 (re-frame/dispatch-sync [:22]))
+
+    (is (= @dispatched-events #{[:later :on [:1]]  [:later :on [:2]]}))))
