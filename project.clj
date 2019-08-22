9ca0910d5084eee8476bb5c907cbeccad6b3bc0e
Isaac Johnston <isaac@superstruct.nz>
2019-08-23 02:39:04 +0300

    Upgrade shadow-cljs to 2.8.51

--- project.clj
+++ project.clj
@@ -2,9 +2,11 @@
   :description  "A re-frame effects handler for listening-for and then post-processing dispatched events."
   :url          "https://github.com/Day8/re-frame-forward-events-fx.git"
   :license      {:name "MIT"}
-  :dependencies [[thheller/shadow-cljs       "2.8.48" :scope "provided"]
-                 [org.clojure/clojure        "1.10.1"  :scope "provided"]
-                 [org.clojure/clojurescript  "1.10.520" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library]]
+                 [thheller/shadow-cljs "2.8.51" :scope "provided"]
                  [re-frame                   "0.10.8" :scope "provided"]]
 
   :profiles {:debug {:debug true}
